f190acd5d2727ad19071a6412e5abbf723af8a3f
jozsefvincze <jozsef.vincze@procats.hu>
2024-06-20 09:47:23 +0200

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
+  get "tags/index"
+  get "tags/show"
+  get "tags/new"
+  get "tags/create"
+  get "tags/edit"
+  get "tags/update"
+  get "tags/destroy"
+  get "categories/index"
+  get "categories/show"
+  get "categories/new"
+  get "categories/create"
+  get "categories/edit"
+  get "categories/update"
+  get "categories/destroy"
+  get "comments/create"
+  get "comments/destroy"
+  get "posts/index"
+  get "posts/show"
+  get "posts/new"
+  get "posts/create"
+  get "posts/edit"
+  get "posts/update"
+  get "posts/destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
